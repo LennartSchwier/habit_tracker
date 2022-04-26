53d9c0052e79c68e201c1df0d5e6b9dbbf52165e
Lennart <lennartschwier@MacBook-Air-von-Lennart.local>
2022-04-26 08:23:47 +0200

    Add user_name params to db_logic.py

--- db_logic.py
+++ db_logic.py
@@ -8,24 +8,24 @@ def connect_to_db(name="main.db"):
     return get_db(name)
 
 
-def add_habit(db, habit: Habit):
-    store_habit_item(db, habit.name, habit.period, habit.deadline)
+def add_habit(db, habit: Habit, user_name: str):
+    store_habit_item(db, habit.name, user_name, habit.period, habit.deadline)
 
 
-def remove_habit(db, name: str):
-    delete_habit_item(db, name)
+def remove_habit(db, name: str, user_name: str):
+    delete_habit_item(db, name, user_name)
 
 
-def update_habit_streaks(db, name: str):
-    habit_entity = get_habit_by_name(db, name)
+def update_habit_streaks(db, name: str, user_name: str):
+    habit_entity = get_habit_by_name(db, name, user_name)
     habit_entity.complete_task()
     update_habit_item(
-        db, habit_entity.name, habit_entity.deadline, habit_entity.current_streak, habit_entity.longest_streak
+        db, habit_entity.name, user_name, habit_entity.deadline, habit_entity.current_streak, habit_entity.longest_streak
     )
 
 
-def get_habit_by_name(db, name: str):
-    habit_item = get_habit_item_by_name(db, name)
+def get_habit_by_name(db, name: str, user_name: str):
+    habit_item = get_habit_item_by_name(db, name, user_name)
     if habit_item is not None:
         return Habit(
             name=habit_item[0][1],
@@ -38,9 +38,9 @@ def get_habit_by_name(db, name: str):
         return None
 
 
-def get_all_habits(db):
+def get_all_habits(db, user_name: str):
     all_habits = []
-    for item in get_all_habit_items(db):
+    for item in get_all_habit_items(db, user_name):
         habit = Habit(item[1], item[2], item[3], item[4], item[5])
         all_habits.append(habit)
     return all_habits
