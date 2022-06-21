f06bde43f24b0881808bd79f3620fb086d0243a7
LennartSchwier <lennart.schwier@icloud.com>
2022-06-21 21:52:05 +0200

    Add docstrings to habit.py

--- habit.py
+++ habit.py
@@ -1,13 +1,62 @@
+"""Contains the Habit class."""
 from datetime import datetime, timedelta
 import uuid
 from custom_exceptions import UpdateActiveStatusError
 
 
 class Habit:
+    """
+    Class to represent a habit.
+
+    Attributes:
+        habit_id: UUID
+            Unique ID to represent the habit
+        name: str
+            The name of the habit
+        created: datetime
+            Date and time of the creation
+        period: int
+            Periodicity in days
+        deadline: datetime
+            Date and time of the deadline
+        is_active: bool
+            Shows if habit is active or inactive
+        current_streak: int
+            Length of the current streak in days (default 0)
+        longest_streak: int
+            Length of the longest streak in days (default 0)
+
+    Methods:
+        complete_task()
+            Updates deadline and streaks according to deadline
+        set_active_status(is_active: bool)
+            Updates the active status and sets the deadline accordingly
+    """
 
     def __init__(self, habit_id: uuid, name: str, created: datetime, period: int, deadline: datetime,
                  is_active: bool, current_streak=0, longest_streak=0
                  ):
+        """
+        Constructor for a Habit instance.
+
+        Params:
+            habit_id: UUID
+                Unique ID to represent the habit
+            name: str
+                The name of the habit
+            created: datetime
+                Date and time of the creation
+            period: int
+                Periodicity in days
+            deadline: datetime
+                Date and time of the deadline
+            is_active: bool
+                Shows if habit is active or inactive
+            current_streak: int
+                Length of the current streak in days (default 0)
+            longest_streak: int
+                Length of the longest streak in days (default 0)
+        """
         assert period >= 1, f"Period {period} has to be greater or equal than one"
 
         # TODO private parameters, timezone
@@ -21,7 +70,7 @@ class Habit:
         self.longest_streak = longest_streak
 
     def complete_task(self):
-        """Updates deadline and streaks according to deadline"""
+        """Updates deadline and streaks according to deadline."""
         if self.__is_within_deadline():
             self.current_streak += 1
             self.deadline += timedelta(days=self.period)
@@ -29,8 +78,20 @@ class Habit:
                 self.longest_streak = self.current_streak
         else:
             self.current_streak = 0
+            self.deadline += timedelta(days=self.period)
 
     def set_active_status(self, is_active: bool):
+        """
+        Updates the active status and sets the deadline accordingly.
+
+        Param:
+            is_active: bool
+                Shows if habit is active or inactive
+
+        Raise:
+            UpdateActiveStatusError
+                Raised if the update has failed
+        """
         previous_status = self.is_active
         self.is_active = is_active
         new_status = self.is_active
@@ -42,7 +103,7 @@ class Habit:
             raise UpdateActiveStatusError
 
     def __is_within_deadline(self):
-        """Private method that returns 'True' if deadline has not passed already"""
+        # Returns True if deadline has not passed already.
         return datetime.now() <= self.deadline
 
     def __repr__(self):
