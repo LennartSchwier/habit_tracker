382fca175583a9854d2b109f610bc5f49764f08d
LennartSchwier <lennart.schwier@icloud.com>
2022-06-23 08:44:44 +0200

    Add docstrings to user.py

--- user.py
+++ user.py
@@ -1,6 +1,31 @@
+"""Contains the User class"""
+
+
 class User:
+    """
+    Class to represent a user.
+
+    Attributes:
+        user_name: str
+            Unique user_name to represent the user
+        password: str
+           Password of the user
+       is_admin: bool
+            Indicates if user is registered as admin (default False)
+    """
 
     def __init__(self, user_name: str, password: str, is_admin="False"):
+        """
+        Constructor for a Habit instance.
+
+        Params:
+        user_name: str
+            Unique user_name to represent the user
+        password: str
+           Password of the user
+       is_admin: bool
+            Indicates if user is registered as admin (default False)
+        """
         self.user_name = user_name
         self.password = password
         self.is_admin = is_admin
