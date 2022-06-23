1d6bf5014d48c479d0d161314649bba89603b199
LennartSchwier <lennart.schwier@icloud.com>
2022-06-23 08:32:42 +0200

    Add docstrings to task.py

--- task.py
+++ task.py
@@ -1,10 +1,29 @@
+"""Contains the Task class"""
 import uuid
 import datetime
 
 
 class Task:
+    """
+    Class to represent a task.
+
+    Attributes:
+        task_id: UUID
+            Unique ID to represent the task
+        created: datetime
+            Tha date and time of creation of the task
+    """
 
     def __init__(self, task_id: uuid, created: datetime):
+        """
+        Constructor for a Task instance.
+
+        Params:
+            task_id: UUID
+                Unique ID to represent the task
+            created: datetime
+                Tha date and time of creation of the task
+        """
         self.task_id = task_id
         self.created = created
 
